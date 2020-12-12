a65950ac35808d9b098338a7a78f8d3c8701e838
Anthony Brown <bruinbrown@gmail.com>
2020-12-12 17:31:19 +0000

    Minor refactor to day 6

--- Day6.fs
+++ Day6.fs
@@ -8,33 +8,20 @@ let private parseInput filePath =
         filePath
         |> File.ReadAllLines
         |> Seq.fold (fun (currentGroup, allGroups) line ->
-            if String.IsNullOrWhiteSpace line then Set.empty, currentGroup::allGroups
-            else
-                let currentGroup =
-                    line
-                    |> Seq.fold (fun s t -> s |> Set.add t) currentGroup
-                currentGroup, allGroups) (Set.empty, [])
-    currentGroup::allGroups
-
-let private calculate = parseInput >> Seq.map Set.count >> Seq.sum
-
-let run filePath =
-    filePath
-    |> calculate
-    |> printfn "Total group count: %i"
-
-let private parseInputPart2 filePath =
-    let currentGroup, allGroups =
-        filePath
-        |> File.ReadAllLines
-        |> Seq.fold (fun (currentGroup, allGroups) line ->
             if String.IsNullOrWhiteSpace line then [], currentGroup::allGroups
             else
                 let currentGroup = (Set.ofSeq line)::currentGroup
                 currentGroup, allGroups) ([], [])
     currentGroup::allGroups
 
-let private calculatePart2 = parseInputPart2 >> Seq.map (Set.intersectMany >> Set.count) >> Seq.sum
+let private calculate = parseInput >> Seq.map (Set.unionMany >> Set.count) >> Seq.sum
+
+let run filePath =
+    filePath
+    |> calculate
+    |> printfn "Total group count: %i"
+
+let private calculatePart2 = parseInput >> Seq.map (Set.intersectMany >> Set.count) >> Seq.sum
 
 let runPart2 filePath =
     filePath
